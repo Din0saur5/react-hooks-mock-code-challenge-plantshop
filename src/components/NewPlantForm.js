fae8f053e564d344f3554da27a6b044ca95023a0
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-01 21:45:58 -0400

    lab complete

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({handleSubmit}) {
+  const [formDeets, setFormDeets] = useState({
+    name:'',
+    image:'',
+    price:0
+
+  })
+  const handleChange = (e) => {
+    const{name, value} = e.target
+    setFormDeets({...formDeets,[name] : value})
+  }
+  
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={(e)=>handleSubmit(e, formDeets)}>
+        <input value={formDeets.name} onChange={handleChange} type="text" name="name" placeholder="Plant name" />
+        <input value={formDeets.image} onChange={handleChange} type="text" name="image" placeholder="Image URL" />
+        <input value={formDeets.price} onChange={handleChange} type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({handleSearch}) {
+const [searchQuery, setSearchQuery] = useState('')
+
+  const controlQuery = (e) => {
+    setSearchQuery(e.target.value)
+     handleSearch(e.target.value)
+  }
+  
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+      value={searchQuery}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e) => controlQuery(e)}
       />
     </div>
   );
